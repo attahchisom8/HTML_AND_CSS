adbd49f5e12e60afb284bb6d3f62f87856cb8dc4
attahchisom8 <attahchisom8@gmail.com>
2026-04-06 15:01:51 +0100

    animated login done successfully

--- animated_login/main.js
+++ animated_login/main.js
@@ -2,10 +2,21 @@ const container = document.getElementById("container");
 const loginBtn = document.getElementById("login");
 const registerBtn = document.getElementById("register");
 
-registerBtn.addEventListener("click", () => {
+registerBtn?.addEventListener("click", () => {
 	container.classList.add("active");
 });
 
-loginBtn.addEventListener("click", () => {
+loginBtn?.addEventListener("click", () => {
 	container.classList.remove("active");
 });
+
+// This will prevent default form submidsion
+const formBtns = document.querySelectorAll("form button");
+
+if (formBtns.length > 0) {
+	formBtns.forEach((btn) => {
+		btn.addEventListener("click", (e) => {
+			e.preventDefault();
+		});
+	});
+};
