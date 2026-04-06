f837f6799eb53879b5b54b209561828d0e1fbaaf
attahchisom8 <attahchisom8@gmail.com>
2026-04-06 12:46:17 +0100

    created the login but still debugging somethings

--- animated_login/main.js
+++ animated_login/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const container = document.getElementById("container");
+const loginBtn = document.getElementById("login");
+const registerBtn = document.getElementById("register");
+
+registerBtn.addEventListener("click", () => {
+	container.classList.add("active");
+});
+
+loginBtn.addEventListener("click", () => {
+	container.classList.remove("active");
+});
