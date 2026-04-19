7e6c9b4b20b087a07c58b429b2d99beeb02a3e1e
attahchisom8 <attahchisom8@gmail.com>
2026-04-19 23:29:40 +0100

    fixed majo bugs

--- simple_calculator/main.js
+++ simple_calculator/main.js
@@ -3,37 +3,61 @@ import { eval_simple_expr } from "./utils/eval.js";
 const buttons = document.querySelectorAll("button");
 const input = document.querySelector("input");
 let isNotOn = false, str = "";
-let res = 0, history = "";
+let res = 0, history = "0";
+let hist_arr = [], idx = 0;
 
 if (buttons.length > 0) {
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
+			let parsedStr = "";
+
       if (["Off", "On"].includes(button.textContent)) {
         isNotOn = !isNotOn;
-        history = "";
+				hist_arr = [];
+				idx = 0;
+				history = "0";
+
         if (isNotOn) {
           str = "";
           button.textContent = "On";
         } else {
           str = "0";
           button.textContent = "Off";
+					hist_arr.push(history);
         }
-        input.value = str;
         input.disabled = isNotOn;
       } else if (button.textContent === "Ac") {
         str = "0";
       } else if (button.textContent === "Del") {
         str = str.substring(0, str.length - 1);
       } else if (button.textContent === "Hist") {
+				if (idx > 0) {
+					history = hist_arr[idx - 1];
+					idx--;
+				}
         str = history;
-      } else if (button.textContent === "=") {
+      } else if (button.textContent === "H ↑") {
+				if (idx + 1 < hist_arr.length) {
+					idx++;
+					history = hist_arr[idx];
+				}
+				str = history;
+			} else if (button.textContent === "=") {
         history = str;
-        res = eval_simple_expr(str);
-        console.log("res: ", res)
+				hist_arr.push(history);
+				idx = hist_arr.length;
+				history = "0";
+				parsedStr = str.replace(/÷/g, "/");
+				parsedStr = parsedStr.replace(/x/g, "*");
+        res = eval_simple_expr(parsedStr);
+        console.log("res: ", res);
         str = res + "";
       } else {  
         str += button.textContent;
-      }
+			}
+
+			if (input.disabled === true)
+				str = "";
       input.value = str;
     })
   });
