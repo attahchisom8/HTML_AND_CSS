2a667cede82d413d8bc7d40ed01982b6a41cb630
attahchisom8 <attahchisom8@gmail.com>
2026-04-21 14:40:04 +0000

    add

--- simple_calculator/main.js
+++ simple_calculator/main.js
@@ -5,75 +5,84 @@ const input = document.querySelector("textarea");
 let isNotOn = false, str = "";
 let res = null, history = "0";
 let hist_arr = [], idx = 0;
-let hasReturned = false;
+let hasReturned = false, hasRes = false;
+
+const handleEvents = (e) => {
+  let parsedStr = "";
+	let btnVal = e.target.textContent;
+
+  if (["Off", "On"].includes(btnVal) || e.key === "Escape") {
+    isNotOn = !isNotOn;
+		hist_arr = [];
+		idx = 0;
+		history = "0";
+		res = null;
+
+    if (isNotOn) {
+      str = "";
+      btnVal = "On";
+    } else {
+      str = "0";
+      btnVal = "Off";
+			hist_arr.push(history);
+    }
+    input.disabled = isNotOn;
+  } else if (btnVal === "Ac" || ["c", "C"].includes(e.key)) {
+    str = "0";
+  } else if (btnVal === "Del" || e.key === "Backspace") {
+    str = str.substring(0, str.length - 1);
+    if (str === "")
+      str = "0";
+  } else if (btnVal === "Hist" || e.key === "ArrowDown") {
+		if (idx > 0) {
+			history = hist_arr[idx - 1];
+			idx--;
+		}
+    str = history;
+  } else if (btnVal === "H↑" || e.key === "ArrowUp") {
+		if (idx + 1 < hist_arr.length) {
+			idx++;
+			history = hist_arr[idx];
+		}
+		str = history;
+	} else if (btnVal === "=" || e.key === "Enter") {
+    history = str;
+		hist_arr.push(history);
+		idx = hist_arr.length;
+		history = "0";
+		parsedStr = str.replace(/÷/g, "/");
+		parsedStr = parsedStr.replace(/x/g, "*");
+		['+', '-', '/', '*', ' of ', '%'].forEach((token) => {
+			if (parsedStr.startsWith(token) && res && hasRes)
+				parsedStr = res + parsedStr;
+				hasRes = false;
+		})
+    res = eval_simple_expr(parsedStr);
+    if (res === undefined)
+      return;
+    str = res + "";
+    hasReturned = true;
+  } else {  
+    str += btnVal || e.key;
+	}
+
+	if (input.disabled === true)
+		str = "";
+	if (str[0] === "0" && !isNaN(str[1]))
+		str = str.slice(1);
+  input.value = str;
+      
+  if (hasReturned) {
+    str = "";
+    hasReturned = false;
+    hasRes = true;
+  }
+}
 
 if (buttons.length > 0) {
   buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-			let parsedStr = "";
-
-      if (["Off", "On"].includes(button.textContent)) {
-        isNotOn = !isNotOn;
-				hist_arr = [];
-				idx = 0;
-				history = "0";
-				res = null;
-
-        if (isNotOn) {
-          str = "";
-          button.textContent = "On";
-        } else {
-          str = "0";
-          button.textContent = "Off";
-					hist_arr.push(history);
-        }
-        input.disabled = isNotOn;
-      } else if (button.textContent === "Ac") {
-        str = "0";
-      } else if (button.textContent === "Del") {
-        str = str.substring(0, str.length - 1);
-      } else if (button.textContent === "Hist") {
-				if (idx > 0) {
-					history = hist_arr[idx - 1];
-					idx--;
-				}
-        str = history;
-      } else if (button.textContent === "H ↑") {
-				if (idx + 1 < hist_arr.length) {
-					idx++;
-					history = hist_arr[idx];
-				}
-				str = history;
-			} else if (button.textContent === "=") {
-        history = str;
-				hist_arr.push(history);
-				idx = hist_arr.length;
-				history = "0";
-				parsedStr = str.replace(/÷/g, "/");
-				parsedStr = parsedStr.replace(/x/g, "*");
-				['+', '-', '/', '*', ' of ', '%'].forEach((token) => {
-				  if (parsedStr.startsWith(token) && res)
-				    parsedStr = res + parsedStr;
-				})
-        res = eval_simple_expr(parsedStr);
-        if (res === undefined)
-          return;
-        str = res + "";
-        hasReturned = true;
-      } else {  
-        str += button.textContent;
-			}
-
-			if (input.disabled === true)
-				str = "";
-			if (str[0] === "0" && !isNaN(str[1]))
-			  str = str.slice(1);
-      input.value = str;
-      
-      if (hasReturned) {
-        str = "";
-        hasReturned = false;
-      }
-    })
+    button.addEventListener("click", handleEvents);
   });
 }
+
+document.addEventListener("keydown", handleEvents);
