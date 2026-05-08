015dc2c49915f2416b7bc4d33a2d7fc68296bef3
attahchisom8 <attahchisom8@gmail.com>
2026-05-08 16:21:28 +0100

    fixing time

--- ExpenseTrackerApp/sortUtility/sortDate.js
+++ ExpenseTrackerApp/sortUtility/sortDate.js
@@ -8,21 +8,40 @@
 
 const getWeeklyRange = (date) => {
   const today = new Date(date);
-  const todayFullTime = today;
+  const todayFullTime = new Date(today);
   today.setHours(0, 0, 0, 0);
   const day = today.getDay();
   const diffToMonday = day === 0 ? 6 : day - 1;
   const monday = new Date(today);
-  monday.setDate(today.getDate() - diffToMonday());
+  monday.setDate(today.getDate() - diffToMonday);
   if (day !== 0) {
-    return { monday, todayFullTime };
+    return { monday, today: todayFullTime, unday: null};
   }
 
   const sunday = new Date(today);
   sunday.setHours(23, 59, 59, 999);
 
-  return {monday, sunday};
+  return {monday, today: null, sunday};
 }
 
 let date = new Date("2026-05-09T12:03:49.725Z");
-console.log(getWeeklyRange(date));
+console.log(date);
+console.log(getWeeklyRange(date))
+
+/**
+ * getYersterdaysDate - get yersterday's date
+ * @date: The given date
+ * 
+ * Return: yersterdays date
+ */
+
+const getYersterdaysDate = (date) => {
+  const yersterday = new Date(date);
+  yersterday.setHours(0, 0, 0, 0);
+  const today = new Date(yersterday);
+  yersterday.setDate(today.getDay() - 1);
+
+  return yersterday;
+}
+console.log("date: ", date);
+console.log(getYersterdaysDate(date));
